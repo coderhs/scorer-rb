3448da9eb36c7322b5e32c69b11b244e946a97a1
Harisankar P S <mailme@hsps.in>
2015-04-04 18:36:07 +0530

    Remove unused score variable

--- lib/scorer.rb
+++ lib/scorer.rb
@@ -5,13 +5,12 @@ class Scorer
   def initialize url = 'http://static.cricinfo.com/rss/livescores.xml'
     @url = url
     @notification = NotificationSystem.new
-    @score = 0
   end
 
   def run
     open(@url) do |f|
       score = Nokogiri::XML(f).xpath("//item//description").text
-      @notification.send_message(score)  and @score = score if score != @score
+      @notification.send_message(score)
     end
   end
 end
