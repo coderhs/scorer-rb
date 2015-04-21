8631503c5d50bd88546f6384caedfc29b04ce971
Harisankar P S <mailme@hsps.in>
2015-04-21 22:25:00 +0530

    Removed demo file

--- demo.rb
+++
@@ -1,8 +0,0 @@
-require 'terminal-notifier'
-require 'open-uri'
-require 'nokogiri'
-url = 'http://static.cricinfo.com/rss/livescores.xml'
-open(url) do |f|
-  msg = Nokogiri::XML(f).xpath("//item//description").last.text
-  TerminalNotifier.notify(msg)
-end
